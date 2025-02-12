895c92554609fba92e145943d8b3b2035c0c3099
Gabriel Cordeiro Barroso Teles <98492418+GabrielCordeiroBarrosoTeles@users.noreply.github.com>
2025-02-12 15:45:19 -0300

    Update atv1.py

--- atv1.py
+++ atv1.py
@@ -2,5 +2,9 @@
 # ## 1. Criando uma função simples
 ## Escreva uma função chamada dobro que receba um número como parâmetro e retorne o dobro desse número.
 
-def dobro(numero):
-    return numero * 2
+def dobro_numero (a):
+    dobro= a*2
+    return dobro
+
+a=int(input("digite um número:"))
+print(dobro_numero(a))
